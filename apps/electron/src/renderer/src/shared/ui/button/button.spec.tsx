165839c464595d0d832d6a82c189b6adc3348812
And Grigoryan <tednaaa@gmail.com>
2023-06-07 00:06:05 +0400

    ci: fix build error

--- apps/electron/src/renderer/src/shared/ui/button/button.spec.tsx
+++ apps/electron/src/renderer/src/shared/ui/button/button.spec.tsx
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react';
 
-import Button from './button';
+import { Button } from './button';
 
 describe('Button', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Button />);
+    const { baseElement } = render(<Button onClick={() => {}}>Test</Button>);
     expect(baseElement).toBeTruthy();
+    expect(baseElement).toContain('Test');
   });
 });
